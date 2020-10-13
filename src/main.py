07e6b632b08ab22485ee8a73709e1445bb5a475e
LucDupuy <luc.dupuy@ryerson.ca>
2020-10-13 14:55:55 -0400

    Playing with a dataset to learn how tensorflow works in terms of binary image classification

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets
 
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+print(y_test)
+
+
+
+
+"""
+
+Classification Array Mapping Object to Index -> ["enemy", "teammate"] would set the label of images with enemies as 0 
+and images with enemies as 1 
+
+"""
