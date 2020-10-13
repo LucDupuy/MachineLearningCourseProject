c538afe755caa97de50fe0bedcf904ea4fc0064b
LucDupuy <luc.dupuy@ryerson.ca>
2020-10-13 16:23:56 -0400

    The model is trained, and I have made notes about what stuff does and how to use the model to predict a label for an image.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
 import train
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import datasets
 
 (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
 x_train, x_test = x_train / 255, x_test / 255
@@ -9,14 +8,14 @@ x_train, x_test = x_train / 255, x_test / 255
 classification_arr = ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
 
 # Building and training the Neural Network
-train.train_model(x_train, y_train, x_test, y_test)
+#train.train_model(x_train, y_train, x_test, y_test)
 
 
 
 
 
 
-"""
+f"""
 
 Normalize the data to between 0 and 1: Take the images (x_train and x_test) and divide by 255 (max RGB value)
 
@@ -27,6 +26,22 @@ Build the Neural Network
 **************************
 
 Convolutional Filters for features (horse has long legs, cat has point ears, plane has wings, truck is bigger then car)
+-> Multiple layers of neurons, ending with a layer of 2 neurons, representing probability of image being something
+-> First layer in input, then are the hidden layers, then the last layer is the output layer
+-> "relu activation function": If value is negative, say 0, if positive, return that value (in contrast to sigmoid 
+function). This is how the neurons works
+
+-> Max Pooling summarizes (downsamples) the layers to make it easier to work with
+
 Max Pooling Layer reduces image to essential information
+
+
+Testing Your Model
+*******************
+After building the neural network, find some test images that the model has not yet seen
+
+prediction = model.predict(np.array([image to test]) / 255)
+index = np.argmax(prediction)
+print(prediction)
 
 """
--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
 
 
 def train_model(x_train, y_train, x_test, y_test):
@@ -12,15 +12,11 @@ def train_model(x_train, y_train, x_test, y_test):
     model.add(layers.Dense(64, activation='relu'))
     # 10 possible Classes
     model.add(layers.Dense(10, activation='softmax'))
-
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # Epochs is how many times the model is going to see the same data
     # Training the model
     model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
-
-    loss, accuracy = model.evaluate(x_test, y_test)
-
     model.save('image_classifier.model')
 
 
